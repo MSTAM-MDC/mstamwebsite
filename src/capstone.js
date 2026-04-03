0c7145b536c3eee0c15f512b28f0e199e278e762
Michael S <99449410+MSTAM-MDC@users.noreply.github.com>
2026-04-03 01:21:50 +0000

    Update 4

--- src/capstone.js
+++ src/capstone.js
@@ -1,5 +1,5 @@
 import React from "react";
 import { render } from "react-dom";
-import CapstoneApp from "./CapstoneApp";
+import CapstoneApp from "./Components/CapstoneApp";
 
 render(<CapstoneApp />, document.getElementById("app"));
